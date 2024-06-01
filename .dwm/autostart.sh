8c9e1b81b3de3195d8100986c06797db419b3e02
stf <sstefanofm@gmail.com>
2024-06-01 19:37:01 -0300

    X apps into autostart_blocking.sh

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
 
-# nitrogen --restore
-dwmblocks &
-picom &
-xrandr --output eDP-1 --right-of HDMI-1
-feh --bg-fill ~/pics/backgrounds/\[black\]eva1.jpg
+xrandr --output eDP-1 --left-of HDMI-1
+feh --bg-fill ~/pics/backgrounds/\[black\]lain0.jpg
---
+++ .dwm/autostart_blocking.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+
+dwmblocks &
+picom --experimental-backends &
