f5b8ef6aff0cf378035e1d0dd68b96acf6e011c3
stf <sstefanofm@gmail.com>
2024-06-11 17:43:54 -0300

    fresh install dwm

---
+++ .local/bin/ri-dwm.sh
@@ -0,0 +1,39 @@
+#! /usr/bin/env bash
+
+#
+# fresh install dwm
+
+set -e
+
+dwm=~/.config/dwm
+
+rmch() {
+  rm -f config.h
+}
+
+rmbdh() {
+  rm -f blocks.def.h
+}
+
+smki() {
+  sudo make clean install
+}
+
+# dwm
+cd $dwm
+rmch
+smki
+rmch
+
+# dmenu
+cd $dwm/dmenu
+rmch
+smki
+rmch
+
+# dwmblocks
+cd $dwm/dwmblocks
+rmbdh
+cp blocks.h blocks.def.h
+smki
+rmbdh
