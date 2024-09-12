1d34874d4c9d68464ac9d90c289c6de7fd88095a
stf <sstefanofm@gmail.com>
2024-09-12 19:59:14 -0300

    update batt.sh

--- .local/bin/batt.sh
+++ .local/bin/batt.sh
@@ -1,5 +1,10 @@
 #! /usr/bin/env bash
 
 batt=$(cat /sys/class/power_supply/BAT0/capacity)
+icon_batt="󰄌 "
 
-echo "$batt% "
+if [ "$(cat /sys/class/power_supply/BAT0/status)" = "Charging" ]; then
+  icon_batt="󱐋 "
+fi
+
+echo "$icon_batt $batt% "
