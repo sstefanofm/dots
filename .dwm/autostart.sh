cca1141748d6c2b1a8c596ad5b823fc0a6348705
stf <sstefanofm@gmail.com>
2024-10-02 17:20:38 -0300

    replaced feh for nitrogen

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
-xrandr --output eDP-1 --left-of HDMI-1
-feh --bg-fill ~/pics/backgrounds/\[black\]lain0.jpg
+# xrandr --output eDP-1 --left-of HDMI-1
+# feh --bg-fill ~/pics/backgrounds/\[black\]lain0.jpg
+nitrogen --restore
