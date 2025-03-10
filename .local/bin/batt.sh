28ea112e031d00e543c7fb09b96a8503eba1e4b8
stf <sstefanofm@gmail.com>
2025-03-10 20:07:34 -0300

    using colorscheme for battery script (dwmblocks)

--- .local/bin/batt.sh
+++ .local/bin/batt.sh
@@ -1,10 +1,18 @@
 #! /usr/bin/env bash
 
-batt=$(cat /sys/class/power_supply/BAT0/capacity)
-icon_batt="󰄌 "
+batt="$(cat /sys/class/power_supply/BAT0/capacity)"
 
+icon_batt="󰄌 "
 if [ "$(cat /sys/class/power_supply/BAT0/status)" = "Charging" ]; then
   icon_batt="󱐋 "
 fi
 
-echo -e "\x03$icon_batt \x01$batt%"
+color="\x06" # SchemeBarError (red)
+if [ "$batt" -ge 61 ]; then
+  color="\x04" # SchemeBarSuccess (green)
+elif [ "$batt" -ge 21 ]; then
+  color="\x05" # SchemeBarWarning (yellow)
+fi
+# \x03 SchemeBarNormal
+
+echo -e "$color$icon_batt \x03$batt%"
