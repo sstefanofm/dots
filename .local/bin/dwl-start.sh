a1174c8f14f61cb67bbd35b00a73b4d71a19c774
Stefano <sstefanofm@gmail.com>
2026-06-23 13:12:37 -0300

    autostart.sh script in dwl's config folder

--- .local/bin/dwl-start.sh
+++ .local/bin/dwl-start.sh
@@ -1,5 +1,6 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 export PATH="$HOME/.local/bin:$PATH"
 export SUDO_ASKPASS="/usr/lib/seahorse/ssh-askpass"
-exec dwl
+
+exec dwl -s "$HOME/.config/dwl/autostart.sh"
